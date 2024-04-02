b0aface731d87e45e14bcc30b91e133fa88feab1
pyam-beep <np03cs4a220081@heraldcollege.edu.np>
2024-04-02 22:44:37 +0545

    FoodPage update

--- src/Pages/FoodPage.jsx
+++ src/Pages/FoodPage.jsx
@@ -3,15 +3,12 @@ import white from '../assets/img/white.jpg'
 
 const FoodPage = () => {
   return (
-    <div className=''>
-        <div>
-            <img src={white} alt="" />
-        </div>
-        <div>
-
+    <div className='absolute inset-0 flex justify-center items-center'>
+        <div className='m-10 flex justify-center'>
+            <img src={white} alt="Bg" className='h-1/2 w-1/2'/>
         </div>
     </div>
   )
 }
 
-export default FoodPage
+export default FoodPage;
